53e28f454ecb1e1c24cc2a697f347ddfcfa79067
Justin <juspua3@gmail.com>
2025-10-30 11:54:08 -0400

    continuing function basics

---
+++ basics.py
@@ -0,0 +1,38 @@
+# Defining a function
+def function_name():
+    pass
+# Calling a function 
+function_name()
+
+def print_header():
+    print("="*20)
+    print("Student Report")
+    print("="*20)
+
+print_header()
+
+def draw_square():
+    for i in range(5):
+        print("*****")
+
+
+draw_square()
+
+def morning_routine():
+    print("Brush teeth")
+    print("Eat breakfast")
+    print("Make my bed")
+
+def school_schedule():
+    print("Biology")
+    print("Gym")
+    print("Financial Markets and Institutions")
+
+def after_school():
+    print("Take a nap")
+    print("Do homework")
+    print("Play Midnight Racing: Tokyo")
+
+morning_routine()
+school_schedule()
+after_school()
