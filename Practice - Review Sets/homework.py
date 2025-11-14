eb7bc7fa702451e8ae77b6b1aaeff563e5e97bcf
Justin <juspua3@gmail.com>
2025-11-14 11:54:20 -0500

    hw done

---
+++ Practice - Review Sets/homework.py
@@ -0,0 +1,35 @@
+#Q1
+# print(update(val)) == 15
+# print(val) == 10
+
+#Q2:
+#print(result) == 10
+
+#Q3
+# print(outer_func(5)) == 10
+
+#Q4
+# print(increment_counter()) == 4
+# print(counter) = 4
+
+#Q5
+# print(reset(score)) == 0
+# print(score) == 50
+
+#Q6
+def square_plus_one(n):
+    return n*n + 1
+
+#Q7
+def register_user(name,age):
+    return f"User {name}, {age} years old, registered"
+
+#Q8
+LIST = "baseball"
+def update_global_list(item):
+    global LIST
+    LIST += "item"
+    return LIST
+#Q9
+def convert_to_fahrenheit(celcius):
+    return (celcius * 9/5 +32)
